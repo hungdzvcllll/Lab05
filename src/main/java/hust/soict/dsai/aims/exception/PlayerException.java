0a16aa677f9893b5e9a07dc4f2242091f5fdcb79
hungdzvcllll <fptjun@gmail.com>
2024-12-21 19:34:49 +0700

    Delete src/main/java/hust/soict/dsai directory

--- src/main/java/hust/soict/dsai/aims/exception/PlayerException.java
+++
@@ -1,3 +0,0 @@
-package hust.soict.dsai.aims.exception;
-public class PlayerException extends Exception{
-}
--- src/main/java/hust/soict/dsai/aims/screen/PlayAble.java
+++
@@ -1,7 +0,0 @@
-package hust.soict.dsai.aims.screen;
-
-import hust.soict.dsai.aims.exception.PlayerException;
-
-public interface PlayAble {
-    public void play() throws PlayerException;
-}
